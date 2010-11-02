f080b2f0ee9b690f8fd7f4fd37d27b8074e9692b
athanassiadis <georgios.athanassiadis@gmail.com>
2010-11-02 15:00:41 -0400

    Added post-comments and simple user authentication

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,7 +2,12 @@ Blog::Application.routes.draw do
   resources :posts
 
   get "home/index"
-
+  
+  resources :posts do
+    resources :comments
+  end
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
